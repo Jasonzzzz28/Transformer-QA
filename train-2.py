41ab61c578892331b0a0ca562f020f7f3e80dfdb
Hzq1941048295 <zh3194@nyu.edu>
2025-05-09 13:48:30 +0800

    Update train-2.py

--- train-2.py
+++ train-2.py
@@ -120,7 +120,7 @@ if __name__ == "__main__":
         model = LlamaForCausalLM.from_pretrained(
             model_name,
             torch_dtype=dtype,
-            device_map="auto",
+            device_map="None",
             use_auth_token=True,
         )
         
