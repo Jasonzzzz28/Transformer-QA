e8999790cb9c90011c894455ad47244401b17f1a
szjiozi <szjiozi@163.com>
2025-05-12 02:27:47 +0000

    update rag module

--- data/rag/knowledge_base.py
+++ data/rag/knowledge_base.py
@@ -18,18 +18,21 @@ def process_knowledge_base(knowledge_base, save_path):
     memory_processor = Base_Memory_3(retrieval_model=retrieval_model)
     memory_processor.process_knowledge_base(knowledge_base, save_path)
 
-test_knowledge_base()
-# if __name__ == "__main__":
-#     with open('/root/autodl-tmp/Transformer-QA/data/offline_data/source_code_qa_with_summary_formatted.json', 'r') as f:
-#         data = json.load(f)
-#     knowledge_base = []
-#     for item in data[:10]:
-#         text = item['question']
-#         knowledge_base.append((text, item['context']))
-#     # with open('test_knowledge_base.json', 'w') as f:
-#     #     json.dump(knowledge_base, f, indent=4)
-#     save_path = "/root/autodl-tmp/QA_memory"
-#     process_knowledge_base(knowledge_base, save_path)
+# test_knowledge_base()
+if __name__ == "__main__":
+    with open('/home/cc/data_pipeline/Transformer-QA/data/offline_data/source_code_qa_with_summary_formatted.json', 'r') as f:
+        qa_from_source_code = json.load(f)
+    with open('/home/cc/data_pipeline/Transformer-QA/data/offline_data/qa_from_commits_formatted.json', 'r') as f:
+        qa_from_commits = json.load(f)
+    data = qa_from_source_code + qa_from_commits
+    knowledge_base = []
+    for item in data:
+        text = item['question']
+        knowledge_base.append((text, item['context']))
+    # with open('test_knowledge_base.json', 'w') as f:
+    #     json.dump(knowledge_base, f, indent=4)
+    save_path = "/mnt/object/QA_memory"
+    process_knowledge_base(knowledge_base, save_path)
 
 
     
